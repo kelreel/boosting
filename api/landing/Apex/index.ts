bdc1209be777b90ea76866820dd4e5efaa621085
Kirill Tyurin <kirill.tiurin@stm-labs.ru>
2021-04-12 09:37:45 +0300

    Fix apex lading api url

--- api/landing/Apex/index.ts
+++ api/landing/Apex/index.ts
@@ -3,6 +3,6 @@ import {CreateRankOrderRequest, ApexRankOrderDetails} from "types/Apex";
 import { OrderDocument } from "types/orders";
 
 export async function createRankOrder(body: CreateRankOrderRequest): Promise<OrderDocument<ApexRankOrderDetails>> {
-    const path = '/landing/apex-legends-legends/rank';
+    const path = '/landing/apex/rank';
     return (await apiClient.post(path, body)).data as OrderDocument<ApexRankOrderDetails>;
 }
